d9240c6db507a3f9a911ea6f626973b496e9dfc3
Hadrien Negros <sorgenh@gmail.com>
2014-02-03 17:00:20 +0100

    Avancement et ajout d'un arff de test.
    Avancement de la classe NaiveBayesMultinomialTER, et ajout d'un fichier
    de test contenant 3 classes et une cinquantaine d'attributs.

--- src/classifiersTER/NaiveBayesMultinomialTER.java
+++ src/classifiersTER/NaiveBayesMultinomialTER.java
@@ -1,8 +1,14 @@
 package classifiersTER;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import weka.classifiers.bayes.NaiveBayesMultinomial;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 /**
 <!-- globalinfo-start -->
@@ -64,7 +70,7 @@ public class NaiveBayesMultinomialTER extends NaiveBayesMultinomial {
 		 * These variables represents the different values that we will need to
 		 * calculate the weighting
 		 */
-		
+
 		double[][] nbOfWordGivenClass = new double[m_numClasses][]; 
 		for(int c = 0; c<m_numClasses; c++)
 		{
@@ -74,7 +80,7 @@ public class NaiveBayesMultinomialTER extends NaiveBayesMultinomial {
 				nbOfWordGivenClass[c][att] = 0;
 			}
 		}
-		
+
 		double[][] nbOfDocsContainingWordGivenClass = new double[m_numClasses][]; 
 		for(int c = 0; c<m_numClasses; c++)
 		{
@@ -84,20 +90,26 @@ public class NaiveBayesMultinomialTER extends NaiveBayesMultinomial {
 				nbOfDocsContainingWordGivenClass[c][att] = 0;
 			}
 		}
-		
+
 		double[] nbOfClassesContainingWord = new double[m_numAttributes]; 
-		for(int c = 0; c<m_numClasses; c++)
+		for(int c = 0; c<m_numAttributes; c++)
 		{
 			nbOfClassesContainingWord[c] = 0;
 		}
-		
+		HashMap<Integer,HashSet<Integer>> classesGivenWord = new HashMap<Integer,HashSet<Integer>>();
+		for(int att = 0; att<m_numAttributes; att++)
+		{
+			classesGivenWord.put(att, new HashSet<Integer>());
+		}		
+
+
 		//Je ne sais pas du tout comment on va faire pour calculer ces 2 prochaines valeurs.
 		double[] nbOfDocsNotInThisClass = new double[m_numClasses]; 
 		for(int c = 0; c<m_numClasses; c++)
 		{
 			nbOfDocsNotInThisClass[c] = 0;
 		}
-		
+
 		double[][] nbOfDocsNotInThisClassContainingWord = new double[m_numClasses][]; 
 		for(int c = 0; c<m_numClasses; c++)
 		{
@@ -107,13 +119,104 @@ public class NaiveBayesMultinomialTER extends NaiveBayesMultinomial {
 				nbOfDocsContainingWordGivenClass[c][att] = 0;
 			}		
 		}
-	
-		
-		//TODO
+
+
+		//enumerate through the instances 
+		Instance instance;
+		int classIndex;
+		double numOccurences;
+		double[] docsPerClass = new double[m_numClasses];
+		double[] wordsPerClass = new double[m_numClasses];
+
+		java.util.Enumeration enumInsts = instances.enumerateInstances();
+		while (enumInsts.hasMoreElements()) 
+		{
+			instance = (Instance) enumInsts.nextElement();
+			classIndex = (int)instance.value(instance.classIndex());
+			docsPerClass[classIndex] += instance.weight();
+
+			for(int a = 0; a<instance.numValues(); a++)
+				if(instance.index(a) != instance.classIndex())
+				{
+					if(!instance.isMissing(a))
+					{
+
+						numOccurences = instance.valueSparse(a) * instance.weight();
+						if(numOccurences < 0)
+							throw new Exception("Numeric attribute values must all be greater or equal to zero.");
+						wordsPerClass[classIndex] += numOccurences;
+						nbOfWordGivenClass[classIndex][instance.index(a)] += numOccurences;
+						if(numOccurences > 0){
+							nbOfDocsContainingWordGivenClass[classIndex][instance.index(a)] += 1;
+							classesGivenWord.get(instance.index(a)).add(classIndex);
+						}
+					}
+				} 
+		}
+
+		displayWeightings(System.out,instances,wordsPerClass,nbOfWordGivenClass,nbOfDocsContainingWordGivenClass,classesGivenWord);
 
 
 
 	}
+	private void displayWeightings(PrintStream out, Instances instances, double[] wordsPerClass,
+			double[][] nbOfWordGivenClass,
+			double[][] nbOfDocsContainingWordGivenClass,
+			HashMap<Integer, HashSet<Integer>> classesGivenWord) {
+		System.out.println("Words Per Class:\n");
+		for(int c = 0; c<m_numClasses; c++)
+		{
 
+			out.println(instances.attribute(instances.classIndex()).value(c) +" : " +wordsPerClass[c]);
+		}
+
+		out.println("\nNb Words given Class:\n");
+		for(int c = 0; c<m_numClasses; c++)
+		{
+			out.println(instances.attribute(instances.classIndex()).value(c)); 
+			for(int att = 0; att<m_numAttributes; att++)
+			{
+				out.println("\t"+instances.attribute(att).name() +" : " +nbOfWordGivenClass[c][att]);
+
+			}
+		}
+		out.println("\nNb Docs containing Word given Class:\n");
+		for(int c = 0; c<m_numClasses; c++)
+		{
+			out.println(instances.attribute(instances.classIndex()).value(c)); 
+			for(int att = 0; att<m_numAttributes; att++)
+			{
+				out.println("\t"+instances.attribute(att).name() +" : " +nbOfDocsContainingWordGivenClass[c][att]);
+
+			}
+		}
+		out.println("\nClasses Given Word:\n");
+		for(int att = 0; att<m_numAttributes; att++)
+		{
+			out.print(instances.attribute(att).name() + " : ");
+			for(int c : classesGivenWord.get(att)){
+				out.print(instances.attribute(instances.classIndex()).value(c) +  " ");
+			}
+			out.println();
+		}
+
+	}
+	/**
+	 * Main method for testing this class.
+	 *
+	 * @param argv the options
+	 * @throws Exception 
+	 */
+	public static void main(String [] argv) {
+		NaiveBayesMultinomialTER test = new NaiveBayesMultinomialTER();
+		try {
+			DataSource ds = new DataSource(argv[1]);
+			Instances i = ds.getDataSet();
+			i.setClassIndex(i.numAttributes()-1);
+			test.buildClassifier(i);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
